aa0a9a2f6dff74489b83c56ff0a1e24b54bb15f5
Vitor <vmuller@alunos.utfpr.edu.br>
2025-04-15 17:15:56 -0300

    End of Section 3

---
+++ client/src/lib/types/index.d.ts
@@ -0,0 +1,12 @@
+type Activity = {
+    id: string
+    title: string
+    description: string
+    date: string
+    category: string
+    isCanceled: boolean
+    city: string
+    venue: string
+    latitude: number
+    longitude: number
+}
