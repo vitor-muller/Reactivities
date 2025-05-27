712e2c7496d31dd8e0c277993d80d6c6215a1d0f
Vitor <vmuller@alunos.utfpr.edu.br>
2025-05-27 20:29:20 -0300

    End of Section 8

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,15 +1,16 @@
 import { Grid2 } from '@mui/material'
 import React from 'react'
 import ActivityList from './ActivityList'
+import ActivityFilters from './ActivityFilters'
 
 export default function ActivityDashboard() {
   return (
     <Grid2 container spacing={3}>
-      <Grid2 size={7}>
+      <Grid2 size={8}>
         <ActivityList />
       </Grid2>
-      <Grid2 size={5}>
-        Activity filters go here
+      <Grid2 size={4}>
+        <ActivityFilters />
       </Grid2>
     </Grid2>
   )
